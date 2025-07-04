812d9590fd53a6019add5edbea16ac95c794d383
juny0 <isjuny0k@gmail.com>
2025-07-04 19:31:24 +0900

    add Board getter setter

--- src/main/java/com/example/gitboard/entitiy/Board.java
+++ src/main/java/com/example/gitboard/entitiy/Board.java
@@ -2,12 +2,18 @@ package com.example.gitboard.entitiy;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Board {
-    @Id @GeneratedValue
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String title;
     private String content;
 }
