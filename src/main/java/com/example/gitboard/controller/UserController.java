ab874f4468e9f3988f9374c855189a354e170e19
juny0 <isjuny0k@gmail.com>
2025-07-07 19:24:52 +0900

    feat: User 엔티티 생성 및 회원가입 API 구현

---
+++ src/main/java/com/example/gitboard/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.example.gitboard.controller;
+
+import com.example.gitboard.dto.SignupRequestDto;
+import com.example.gitboard.entity.User;
+import com.example.gitboard.repository.UserRepository;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping("/signup")
+    public String signup(@RequestBody @Valid SignupRequestDto requestDto) {
+        String encodedPassword = passwordEncoder.encode(requestDto.getPassword());
+        User user = new User(requestDto.getUsername(), encodedPassword);
+        userRepository.save(user);
+        return "회원가입 완료";
+    }
+}
